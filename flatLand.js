734dc45d101fb4970e68e13650f85dc21764beaf
tyler jensen <tylerjensen1107@gmail.com>
2015-03-03 16:13:34 -0800

    Changed more stuff, added inputs

--- flatLand.js
+++ flatLand.js
@@ -1,8 +1,20 @@
+//Variables that effect the visual
+var xRotate = .01;
+var zRotate = .01;
+var yRotate = .01;
+//How much shift there is after the circle starts to repeat from the center
+var DIFF_CONSTANT = .05;
+//Shift of each line on each tick
+var DIFF = Math.random() * 3.14;
+var TIGHTNESS = Math.random();
+var DELETE = false;
+var counterMax = 1000 * DIFF;
+var SYNCMUSIC = false;
 
 var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
 var analyser = audioCtx.createAnalyser();
 
-console.log(analyser);
+addListeners();
 
 var song = document.getElementById("music");
 var bufferLength;
@@ -54,21 +66,7 @@ var oldVectors = new Array();
 for(var i = 0; i < colors.length; i++) 
     oldVectors.push(new THREE.Vector3(0,0,0));
 
-console.log(scene);
 
-var xRotate = 0.01;
-var zRotate = 0.01;
-var yRotate = 0.01;
-//How much shift there is after the circle starts to repeat from the center
-var DIFF_CONSTANT = .05;
-//Shift of each line on each tick
-var DIFF = Math.random() * 3.14;
-var TIGHTNESS = Math.random();
-var DELETE = false;
-var counterMax = 1000 * DIFF;
-var SYNCMUSIC = false;
-//Inverse relationship
-var DISTANCE = 1000 * DIFF;
 
 
 function render() {
@@ -92,13 +90,12 @@ function render() {
     var averageFreq = sumFreq / dataArray.length;
 
     if(SYNCMUSIC)
-        sumFreq = averageFreq / 32;
+        sumFreq =  4 * averageFreq / 128;
     else
         sumFreq = 4;
 
 
     requestAnimationFrame( render );
-    console.log(counter);
     if(counter >= counterMax) {
         counter = counter % counterMax + DIFF_CONSTANT;
         for(var i = 0; i < colors.length; i++) 
@@ -108,14 +105,13 @@ function render() {
             colors[i] = getRandomColor();
         DIFF = Math.random() * 3.14;
         counterMax = 1000 * DIFF;
-        DISTANCE = 1000 * DIFF;
     } else {
         for(var i = 0; i < colors.length; i++) {
             var geometry = new THREE.Geometry();
             var vector1 = oldVectors[i];
             var vector2 = vector1.clone();
-            vector2.x = (vector2.x + Math.sin(counter + i) / TIGHTNESS) * counter / DISTANCE;
-            vector2.y = (vector2.y + Math.cos(counter + i) / TIGHTNESS) * counter / DISTANCE;
+            vector2.x = (vector2.x + Math.sin(counter + i) / TIGHTNESS) * counter / counterMax;
+            vector2.y = (vector2.y + Math.cos(counter + i) / TIGHTNESS) * counter / counterMax;
             oldVectors[i] = vector2;
             geometry.vertices.push(vector1);
             geometry.vertices.push(vector2);
@@ -131,6 +127,7 @@ function render() {
         scene.scale.x = sumFreq;
         scene.scale.y = sumFreq;
         counter += DIFF;
+        console.log(DIFF);
     }
 
     if(DELETE) {
@@ -146,8 +143,6 @@ function render() {
 
 
 }
-
-render();
 
 function getRandomColor() {
     var letters = '0123CDEF'.split('');
@@ -171,5 +166,29 @@ function onDocumentMouseMove( event ) {
     mouseX = event.clientX - windowHalfX;
     mouseY = event.clientY - windowHalfY;
 
+}
+
+function addListeners() {
+    document.getElementById("newShape").addEventListener("click", changeShape);
+    document.getElementById("refresh").addEventListener("click", changeVisual);
+}
+
+function changeShape() {
+    DIFF = Math.random() * 3.14;
+}
+
+function changeVisual() {
+    xRotate = document.getElementById("xRot").value || .01;
+    yRotate = document.getElementById("yRot").value || .01;
+    zRotate = document.getElementById("zRot").value || .01;
+    DIFF = document.getElementById("Shape").value || Math.random() * 3.14;
+    TIGHTNESS = document.getElementById("Tightness").value || Math.random();
+    DELETE = document.getElementById("Delete").value;
+    SYNCMUSIC = document.getElementById("Sync").value;
+    counterMax = document.getElementById("CounterMax").value || 1000 * DIFF;
+    console.log(DIFF);
+
+    render();
+    
 }
 
